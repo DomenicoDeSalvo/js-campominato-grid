0ccc3a77341c926e9df5b6942a0bbd60f574c139
DomenicoDeSalvo <domenicodesalvo_96@yahoo.it>
2024-03-13 20:55:31 +0100

    Added code to create different number of cells depending on the level

--- js/app.js
+++ js/app.js
@@ -9,17 +9,31 @@ let difficulty = '';//string
 // La griglia sarà formata da quadrati uguali che si troveranno all'interno del div con classe grid del DOM
 // Acquisizione dell div.
 const gridElement = document.querySelector('.grid');//Element||Null
-//Creazione del div che andrà inserito nella griglia.
-const cellElement = document.createElement('div'); //Object
-//Assegnazione al div della classe "cell"
-cellElement.className = 'cell';
-//All'interno di ogni quadrato ci sarà il numero che indica la sua poszione nella griglia.
-let num = ''
-cellElement.innerHTML = num;
 
 //Assegnazione del valore al click.
 submitElement.addEventListener('click', function(){
     difficulty = difficultyElement.value;//string
+
+    // In base alla difficoltà verrà generata una griglia di dimensioni diverse.
+    for(let i = 0; i < gridSize(difficulty); i++){
+
+        //Creazione del div che andrà inserito nella griglia.
+        const cellElement = document.createElement('div'); //Object
+        //Assegnazione al div della classe "cell"
+        cellElement.className = 'cell';
+        //All'interno di ogni cella ci sarà un numero corrispondente alla sua posizione nella griglia.
+        const num = i + 1; //Number
+        cellElement.innerHTML = num;
+        //A Diversa difficoltà corrisponde una diversa quantità di celle, che quindi avranno dimensioni diverse.
+        if(difficulty === '10') {
+            cellElement.classList.add('difficulty1');
+        } else if(difficulty === '9') {
+            cellElement.classList.add('difficulty2');
+        } else {
+            cellElement.classList.add('difficulty3');
+        }
+
+    }
     
 })
 
