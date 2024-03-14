57c6671c05cb703524ac30aa55bea4d134aace06
DomenicoDeSalvo <domenicodesalvo_96@yahoo.it>
2024-03-14 10:12:08 +0100

    Changed the way the game is resetted

--- js/app.js
+++ js/app.js
@@ -9,20 +9,17 @@ let difficulty = '';//string
 // La griglia sarà formata da quadrati uguali che si troveranno all'interno del div con classe grid del DOM
 // Acquisizione dell div.
 const gridElement = document.querySelector('.grid');//Element||Null
-
+generateGrid()
 //Assegnazione del valore al click.
-submitElement.addEventListener('click', function(){
+submitElement.addEventListener('click', generateGrid);
+function generateGrid(){
+    gridElement.innerHTML = ('');
+
     difficulty = difficultyElement.value;//string
-
-    // NOTA PER CHI CORREGGE: Non so se questa cosa sia corretta, ma è l'unico modo che mi viene in mente per evitare che, cliccando più volte sul bottone play, si creino più griglie una sotto l'altra.
-    //Far scomparire la selezione della difficoltà ed il bottone play per evitare che vengano create più griglie.
-    const difficultySelectionElement = document.querySelector('.difficulty_selection'); //Element || Null
-    difficultySelectionElement.classList.add('hide'); 
-
-
+    
     // In base alla difficoltà verrà generata una griglia di dimensioni diverse.
     for(let i = 0; i < gridSize(difficulty); i++){
-
+        
         //Creazione del div che andrà inserito nella griglia.
         const cellElement = document.createElement('div'); //Object
         //Assegnazione al div della classe "cell"
@@ -38,20 +35,21 @@ submitElement.addEventListener('click', function(){
         } else {
             cellElement.classList.add('difficulty3');
         }
-
+        
         //Inserire le celle nel DOM.
         gridElement.append(cellElement);
-
+        
         //Quando si clicca su una cella essa si colora di azzurro, cliccando una seconda volta il colore torna normale.
         cellElement.addEventListener('click', function(){
             cellElement.classList.toggle('colored');
             //Viene stampato un console log in cui si dichiare quale cella è stata cliccata.
             console.log('È stata cliccata la cella numero ' + num);
         })
-
+        
     }
+}
     
-})
+
 
 // FUNZIONI
 //Funzione che avrà il compito di determinare la grandezza della griglia.
